bc3d6161c57328b6064e8383f441aa90a80bd8d0
Duncan Champney <DuncanC@aol.com>
2015-04-21 08:13:41 -0400

    Initial Commit

---
+++ SwiftTestProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftTestProject
+//
+//  Created by Duncan Champney on 4/21/15.
+//  Copyright (c) 2015 Duncan Champney. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
